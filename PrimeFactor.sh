2ac402a5027fce7a4e6734f2c170fff3f4c747c5
dhanashripatil205 <dhanashripatil205@gmail.com>
2022-12-17 14:41:36 +0530

    added: prime factorization

---
+++ PrimeFactor.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -x
+
+echo "Program to find prime factor of "
+ read -p "Enter the number: " num
+
+fact=1
+
+echo -n "Prime factor of $num are : "
+
+for (( i=2; i*i<=num; i++ ))
+do
+
+	rem=$((num%i))
+
+for (( ; $rem==0; ))
+do
+	num=$((num/i))
+	rem=$((num%i))
+	echo -n "$i "
+done
+
+done
+
+if [ $num -ne 1 ]
+then
+echo -n "$num "
+fi
+
+
+
+#echo "Program to find prime factor of a number."
+#read -p "Enter a number : " num
+#fact=1
+#echo -n "Prime factors of $num are: "
+#for (( i=2; i*i<=num; i++ ))
+#do
+#	rem=$((num%i))
+#	for (( ; $rem==0; ))
+#	do
+#		num=$((num/i))
+#		rem=$((num%i))
+#		echo -n "$i "
+#	done
+#done
+#if [ $num -ne 1 ]
+#then
+#	echo -n "$num "
+#fi
